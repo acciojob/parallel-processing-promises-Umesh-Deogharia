4d9a66a3b82e97d328821f12fbc69bd5426da1e4
Umesh Deogharia <143274287+Umesh-Deogharia@users.noreply.github.com>
2025-02-19 17:39:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,31 @@
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
+output.innerHTML = `<div id="loading">Loading</div>`
+
 const images = [
   { url: "https://picsum.photos/id/237/200/300" },
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+let p1 = new Promise((resolve, reject) => { 
+   return resolve(images[0]);
+})
+let p2 = new Promise((resolve, reject) => { 
+   return resolve(images[1]);
+})
+let p3 = new Promise((resolve, reject) => { 
+    resolve(images[2]);
+})
+
+let Promises = [p1, p2, p3];
+
+Promise.all(Promises).then(() => {
+    output.innerHTML = "";
+    output.innerHTML = "All images downloaded successfully!";
+}).catch(() => {
+    output.innerHTML = "";
+    output.innerText = "Images has not downloaded";
+    
+})
